cbd3af895d7207ed7c9372eda8670f4dc38929a5
karsten zakarias <k_zakarias@hotmail.com>
2017-11-07 11:36:38 +0100

    init

---
+++ src/Home.vue
@@ -0,0 +1,24 @@
+<<template>
+    <div>
+        <p>Server status: {{ status }}</p>
+        <hr>
+        <button v-on:click="changeStatus">Change Status</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus(){
+                this.status = 'Normalised';
+            }
+        }
+    }
+</script>
+
